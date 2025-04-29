6846379ecd0f8339bca26e1d8517e22440e3f684
Matula José Alberto <69260911+Chancilson@users.noreply.github.com>
2025-04-29 03:38:25 +0000

    adicionado script `lint:check` e `lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Olá, este ano de 2025 eu vou viver de forma definitiva na europa. Deus no comando. 🔥🔥🔥🔥</h1>;
+  return (
+    <h1>
+      Olá, este ano de 2025 eu vou viver de forma definitiva na europa. Deus no
+      comando. 🔥🔥🔥🔥
+    </h1>
+  );
+}
+
+function test() {
+  console.log("Chancilson o fodido");
+}
+
+function venezuela() {
+  console.log("Chancilson builing");
 }
 
 export default Home;
